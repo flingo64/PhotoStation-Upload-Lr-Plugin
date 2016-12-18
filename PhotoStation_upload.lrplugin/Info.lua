53a3bfc5674c4776600088266888af70c5b979e4
Martin <github@messmer-online.de>
2016-12-19 00:04:34 +0100

    Version 6.1.7

--- PhotoStation_upload.lrplugin/Info.lua
+++ PhotoStation_upload.lrplugin/Info.lua
@@ -40,8 +40,8 @@ of it requires the prior written permission of Adobe.
 ------------------------------------------------------------------------------]]
 plugin_major = 6
 plugin_minor = 1
-plugin_rev = 6
-plugin_build = 20161211
+plugin_rev = 7
+plugin_build = 20161219
 pluginVersion = plugin_major .. '.' .. plugin_minor .. '.' ..plugin_rev .. '.' .. plugin_build
 plugin_TkId = 'de.messmer-online.lightroom.export.photostation_upload'
 
