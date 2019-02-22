25383296a345147f1b8da0b6357c8dfa9ae77426
flingo64 <flingo64@users.noreply.github.com>
2019-02-22 20:03:41 +0100

    Use JSON-decode w/ error-handler and add. info

--- PhotoStation_upload.lrplugin/PSExiftoolAPI.lua
+++ PhotoStation_upload.lrplugin/PSExiftoolAPI.lua
@@ -281,7 +281,7 @@ function PSExiftoolAPI.queryLrFaceRegionList(h, photoFilename)
 		return nil
 	end
 	
-	local results = JSON:decode(queryResults)
+	local results = JSON:decode(queryResults, "PSExiftoolAPI.queryLrFaceRegionList(" .. photoFilename .. ")")
 	if not results or #results < 1 then
 		writeLogfile(3, "PSExiftoolAPI.queryLrFaceRegionList: JSON decode of results failed\n")
 		return nil
