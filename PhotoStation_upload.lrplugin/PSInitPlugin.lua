588e3dd1fb57fb0e165b2b5327d4a542c413c917
flingo64 <flingo64@users.noreply.github.com>
2019-02-24 17:27:46 +0100

    Moved loading of video presets to init routine
    Avoid error message "AgPreference: can't store non-scalar values' when changing in Export Dialiog to Photo StatLr

---
+++ PhotoStation_upload.lrplugin/PSInitPlugin.lua
@@ -0,0 +1,84 @@
+--[[----------------------------------------------------------------------------
+
+PSInitPlugin.lua
+This file is part of Photo StatLr - Lightroom plugin.
+Copyright(c) 2019, Martin Messmer
+
+plugin initialization:
+	- load Lr video presets
+	
+Photo StatLr is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Photo StatLr is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Photo StatLr.  If not, see <http://www.gnu.org/licenses/>.
+
+Photo StatLr uses the following free software to do its job:
+	- convert.exe,			see: http://www.imagemagick.org/
+	- ffmpeg.exe, 			see: https://www.ffmpeg.org/
+	- qt-faststart.exe, 	see: http://multimedia.cx/eggs/improving-qt-faststart/
+]]
+--------------------------------------------------------------------------------
+
+-- Lightroom SDK
+local LrExportSettings 	= import 'LrExportSettings'
+local LrPathUtils 		= import 'LrPathUtils'
+
+require "PSUtilities"
+
+--============================================================================--
+
+-- check if my custom video output presets are already installed 
+local myVideoExportPresets = LrExportSettings.videoExportPresetsForPlugin( _PLUGIN )
+
+if myVideoExportPresets and #myVideoExportPresets > 0 then
+   	-- make sure, no older version of Video presets are installed
+	LrExportSettings.removeVideoExportPreset('ALL', _PLUGIN)
+--		writeLogfile(2, 'PSInitPlugin: found my custom video export presets.\n')
+  	end
+ 
+ 	-- (re-)install my video export presets 	
+local pluginDir = _PLUGIN.path
+local presetFile = 'OrigSizeHiBit.epr'
+local presetFile2 = 'OrigSizeMedBit.epr'
+--	writeLogfile(2, 'PSInitPlugin: adding video export preset ' ..  LrPathUtils.child(pluginDir, presetFile) .. '\n')
+
+local origSizeVideoPreset = LrExportSettings.addVideoExportPresets( {
+	[ 'Original Size - High Bitrate' ] = {
+		-- The format identifier for the video export format that this preset
+		-- corresponds to. This identifier must be equal to the 'formatName'
+		-- entry in one of the entries in the table returned by
+		-- LrExportSettings.supportableVideoExportFormats, i.e. 'h.264'.
+		format = 'h.264',
+
+		-- Must be an absolute path
+		presetPath = LrPathUtils.child(pluginDir, presetFile),
+
+		-- To be displayed as target info in export dialog.
+		targetInfo =  LOC "$$$/PSUpload/ExportDialog/VideoSection/OrigSizePreset/TargetInfo=original resolution, high bitrate",
+ 	}, 
+
+	[ 'Original Size - Medium Bitrate' ] = {
+		format = 'h.264',
+
+		-- Must be an absolute path
+		presetPath = LrPathUtils.child(pluginDir, presetFile2),
+
+		-- To be displayed as target info in export dialog.
+		targetInfo =  LOC "$$$/PSUpload/ExportDialog/VideoSection/OrigSizePreset2/TargetInfo=original resolution, medium bitrate",
+ 	}, 
+ },	_PLUGIN
+)
+
+if not origSizeVideoPreset then
+	writeLogfile(1, "PSInitPlugin: Could not add custom video export presets!\n")
+else
+	writeLogfile(2, "PSInitPlugin: Successfully added " .. #origSizeVideoPreset .. " custom video export presets.\n")
+end
