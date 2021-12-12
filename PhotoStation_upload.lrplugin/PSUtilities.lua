9978a956ff6c1ace3c07398af4cbd33050eedd43
Martin <martin.messmer@localhost>
2021-12-12 23:19:23 +0100

    urlencode() optimized

--- PhotoStation_upload.lrplugin/PSUtilities.lua
+++ PhotoStation_upload.lrplugin/PSUtilities.lua
@@ -677,8 +677,7 @@ end
 function urlencode(str)
 	if (str) then
 		str = string.gsub (str, "\n", "\r\n")
-		str = string.gsub (str, "([^%w ])",function (c) return string.format ("%%%02X", string.byte(c)) end)
-		str = string.gsub (str, " ", "%%20")
+		str = string.gsub (str, "([%W])",function (c) return string.format ("%%%02X", string.byte(c)) end)
 	end
 	return str
 end 
