b1efcc2bcc064d9154e01a42a68a8b92673356d8
Martin <martin@home>
2026-02-06 18:58:07 +0100

    Updated copyright

--- PhotoStation_upload.lrplugin/PSPluginTagsetCompact.lua
+++ PhotoStation_upload.lrplugin/PSPluginTagsetCompact.lua
@@ -2,7 +2,7 @@
 
 PSPluginTagsetCompact.lua
 This file is part of Photo StatLr - Lightroom plugin.
-Copyright(c) 2015-2025, Martin Messmer
+Copyright(c) 2015-2026, Martin Messmer
 
 Summary information for Photo StatLr
 
--- PhotoStation_upload.lrplugin/PSUpdate.lua
+++ PhotoStation_upload.lrplugin/PSUpdate.lua
@@ -2,7 +2,7 @@
 
 PSUpdate.lua
 This file is part of Photo StatLr - Lightroom plugin.
-Copyright(c) 2015-2025, Martin Messmer
+Copyright(c) 2015-2026, Martin Messmer
 
 Photo StatLr update check:
 	- checkForUpdate()
